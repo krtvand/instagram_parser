6f4055431763497fddbbc5e0c3644df08445a0cd
Your Name <you@example.com>
2018-02-25 11:26:44 +0300

    парсинг queryhash done

--- instagram_parser/crawler/crawler/query_hash_extractor.py
+++ instagram_parser/crawler/crawler/query_hash_extractor.py
@@ -7,6 +7,8 @@ def get_link_for_js_file_with_queryhash(response):
     return link
 
 def get_queryhash_from_js_file(response):
-    pattern = '(.*)(?P<text_before>locationPosts\.byLocationId\.get\(e\)\.pagination},queryId:\")(?P<query_hash>.*?)(\".*)'
+    pattern = r'(.*)(?P<text_before>locationPosts\.byLocationId\.get\(e\)\.pagination},queryId:\")(?P<query_hash>.*?)(\".*)'
+    match = re.match(pattern, response.text)
+    query_hash = match.group('query_hash')
 
-    return ''
+    return query_hash
