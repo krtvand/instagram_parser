5d03d880c35abbc96404b204ac406de8bc0d7959
Your Name <you@example.com>
2018-03-03 00:13:00 +0300

    парсинг постов, пагинация готовы

--- instagram_parser/crawler/crawler/spiders/example.py
+++ instagram_parser/crawler/crawler/spiders/example.py
@@ -1,7 +1,7 @@
 import scrapy
 from scrapy import Request
 
-from instagram_parser.crawler.crawler.pagination import (PaginatorInFirstPage, PaginatorInNextPage)
+from instagram_parser.crawler.crawler.pagination import (Paginator, PaginatorInFirstPage, PaginatorInNextPage)
 from instagram_parser.crawler.crawler.data_extractor import (FirstPageParser, NextPageParser)
 
 
@@ -12,35 +12,56 @@ class ExampleSpider(scrapy.Spider):
     start_urls = ['{}/explore/locations/{}/'.format(base_url, location_id)]
     index_page_parser = FirstPageParser()
     next_page_parser = NextPageParser()
-    index_page_paginator = PaginatorInFirstPage(base_url, location_id)
     next_page_paginator = PaginatorInNextPage(base_url, location_id)
     posts_info = []
+    paginator = None
+    page_parser = None
+
+    def set_paginator(self, paginator_type):
+        paginators = {
+            'index_page_paginator': PaginatorInFirstPage(self.base_url, self.location_id),
+            'next_page_paginator': PaginatorInNextPage(self.base_url, self.location_id)
+        }
+        self.paginator = paginators[paginator_type]
+
+    def set_page_parser(self, parser_type):
+        parsers = {
+            'index_page_parser': FirstPageParser(),
+            'next_page_parser': NextPageParser()
+        }
+        self.page_parser = parsers[parser_type]
 
     def parse(self, response):
-        shared_data = self.index_page_parser.extract_shared_data(response)
-        posts_list = self.index_page_parser.get_post_objects(shared_data)
+        self.set_paginator('index_page_paginator')
+        self.set_page_parser('index_page_parser')
+        shared_data = self.page_parser.extract_shared_data(response)
+        posts_list = self.page_parser.get_post_objects(shared_data)
         for post in posts_list:
-            self.posts_info.append(self.index_page_parser.collect_data_from_post(post))
+            self.posts_info.append(self.page_parser.collect_data_from_post(post))
 
         # pagination
-        if self.index_page_paginator.pagination_has_next_page(shared_data):
-            next_page_url = self.index_page_paginator.get_url_for_next_page(response, shared_data)
+        if self.paginator.pagination_has_next_page(shared_data):
+            next_page_url = self.paginator.get_url_for_next_page(response, shared_data)
             headers = {'x-requested-with': 'XMLHttpRequest'}
-            yield Request(next_page_url, headers=headers, callback=self.parse_next_page)
+            meta = response.request.meta
+            yield Request(next_page_url, headers=headers, meta=meta, callback=self.parse_next_page)
         else:
             yield {'posts_info': self.posts_info}
 
     def parse_next_page(self, response):
-        shared_data = self.next_page_parser.extract_shared_data(response)
-        posts_list = self.next_page_parser.get_post_objects(shared_data)
+        self.set_paginator('next_page_paginator')
+        self.set_page_parser('next_page_parser')
+        shared_data = self.page_parser.extract_shared_data(response)
+        posts_list = self.page_parser.get_post_objects(shared_data)
         for post in posts_list:
-            self.posts_info.append(self.next_page_parser.collect_data_from_post(post))
+            self.posts_info.append(self.page_parser.collect_data_from_post(post))
 
         # pagination
-        if self.next_page_paginator.pagination_has_next_page(shared_data):
-            next_page_url = self.next_page_paginator.get_url_for_next_page(response, shared_data)
+        if self.paginator.pagination_has_next_page(shared_data):
+            next_page_url = self.paginator.get_url_for_next_page(response, shared_data)
             headers = {'x-requested-with': 'XMLHttpRequest'}
-            yield Request(next_page_url, headers=headers, callback=self.end)
+            meta = response.request.meta
+            yield Request(next_page_url, headers=headers, meta=meta, callback=self.end)
         else:
             yield {'posts_info': self.posts_info}
 
