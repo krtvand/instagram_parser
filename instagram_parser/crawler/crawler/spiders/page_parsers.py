97abb9d8d5405661820c37cdc87454d35e635dfe
andrey kartaev <andrey.kartaev@simbirsoft.com>
2018-03-07 17:55:00 +0300

    новая сущность - парсер страницы -f

--- instagram_parser/crawler/crawler/spiders/page_parsers.py
+++ instagram_parser/crawler/crawler/spiders/page_parsers.py
@@ -7,14 +7,21 @@ from instagram_parser.crawler.crawler.data_extractors import (FirstPageDataExtra
 from instagram_parser.crawler.crawler.spider_stopper import SpiderStopper
 
 
+class StopCrawlerException(Exception):
+    """
+    Событие остановки парсинга
+    """
+
+
 class SearchResultPagePostsParser:
     """
     Парсинг постов со страницы результатов поиска по локации или тэгу
     """
 
-    def __init__(self, spider_stopper: SpiderStopper):
+    def __init__(self, spider_stopper: SpiderStopper, base_url, ):
         self.spider_stoper = spider_stopper
         self.data_extractor = self.create_data_extractor()
+        self.base_url = base_url
 
     def parse(self, response: scrapy.http.Response):
         parsed_posts_data = {}
@@ -22,6 +29,9 @@ class SearchResultPagePostsParser:
         posts_list = self.data_extractor.get_post_objects(shared_data)
         for post in posts_list:
             post_data = self.data_extractor.collect_data_from_post(post)
+            (post_id, post_info), = post_data.items()
+            if self.spider_stoper.should_we_stop_spider(post_info['publication_date']):
+                raise StopCrawlerException
             parsed_posts_data.update(post_data)
 
         return parsed_posts_data
